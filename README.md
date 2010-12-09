2455c2931309c71b034156e5e4598167ba9d4227
Vern Sun <s5unty@gmail.com>
2010-12-09 23:01:00 +0800

    在服务器端获取 tweets 并更新静态页面，避免了在墙内不能显示 tweets 的问题

--- README.md
+++ README.md
@@ -8,15 +8,6 @@ title: README
 It's automatically transformed by [Jekyll](http://github.com/mojombo/jekyll)
 into a static site whenever I push this repository to GitHub.
 
-# License #
-
-The following directories and their contents are Copyright &copy; Ali Polatel.
-You may not reuse anything therein without my permission unless stated
-otherwise:
-
-- \_posts/
-- audio/
-
-All other directories are public domain. Feel free to use the HTML and CSS as you please.
+    echo "2 */30 * * * * /bin/bash /sun/blog/_scripts/tweets.sh" >> /etc/crontab
 
 <!-- vim: set tw=80 ft=mkd spell spelllang=en sw=4 sts=4 et : -->
---
+++ _scripts/tweets.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+rt() {
+    time=$(date -d "${1}" +%s)
+    curr=$(date +%s)
+    shift=$(($curr - $time));
+
+	if [ $shift -lt 45 ]; then
+		diff="$shift"
+		term="second" # 45 秒前
+	elif [ $shift -lt 2700 ]; then
+        diff="$(($shift / 60))"
+		term="minute" # 45 分钟前
+	elif [ $shift -lt 64800 ]; then
+        diff="$(($shift / 60 / 60))"
+		term="hour" # 18 小时前
+    elif [ $shift -lt 518400 ]; then
+        diff="$(($shift / 60 / 60 / 24))"
+		term="day" # 6 天前
+    elif [ $shift -lt 1814400 ]; then
+        diff="$(($shift / 60 / 60 / 24))"
+		term="week" # 3 周前 (21 天前)
+    fi
+
+	if [ $diff -gt 1 ]; then term=$term"s"; fi
+	echo "$diff $term ago";
+}
+
+xml=$(curl -s --basic http://twitter.com/statuses/user_timeline/s5unty.rss?count=1)
+date=$(grep pubDate <<< "$xml" | tr -d '\t' | sed 's/^<.*>\([^<].*\)<.*>$/\1/' | sed 's/<pubDate>//' | sed 's/<\/pubDate>//' | sed 's/^ //g')
+desc=$(awk -v "tag=description" '$0 ~ tag && a++ { print; exit }' <<< "$xml" | tr -d '\t' | sed 's/^<.*>\([^<].*\)<.*>$/\1/' | sed 's/<description>//' | sed 's/<\/description>//' | sed 's/CorinCole: //' | sed 's/^ //g')
+link=$(awk -v "tag=link" '$0 ~ tag && 2 ==   a++ { print; exit }' <<< "$xml" | tr -d '\t' | sed 's/^<.*>\([^<].*\)<.*>$/\1/' | sed 's/<link>//' | sed 's/<\/link>//' | sed 's/^ //g')
+
+date=$(rt "${date}")
+desc=$(echo $desc | sed 's|[[:lower:]]\{1,\}://[^[:space:]]\{1,\}|<a href="&">&</a>|g ; s| \(www.[^[:space:]]*\)| <a href="\1">\1</a>|g')
+desc=$(echo $desc | sed 's|^s5unty: ||')
+
+read  -rd '' desc <<< "$desc"
+read  -rd '' link <<< "$link"
+
+for file in "/sun/blog/site/index.html"
+do
+  perl -pi -e 'BEGIN { $x = shift; $y = shift; } s/$x/$y/g' -- "<span id=\"tweets\">.*</span>" "<span id=\"tweets\">$desc</span>" $file
+  perl -pi -e 'BEGIN { $x = shift; $y = shift; } s/$x/$y/g' -- "<span id=\"tweets_time\">.*</span>" "<span id=\"tweets_time\"><a href=\"$link\">$date</a></span>" $file
+done
