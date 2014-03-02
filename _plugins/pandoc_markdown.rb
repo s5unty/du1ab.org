2cae17e9cb0b0259b7ec9acdde495865dcf9bc0f
Vern Sun <s5unty@gmail.com>
2014-03-02 18:06:11 +0800

    追加了 Asciidoc 解释器、更新了 Markdown 解释器 (pandoc)。

---
+++ _plugins/pandoc_markdown.rb
@@ -0,0 +1,10 @@
+require 'pandoc-ruby'
+
+class Jekyll::MarkdownConverter
+
+  def convert(content)
+    return super unless ( @config['markdown'] == 'pandoc' || @config['pandoc'] )
+    @pandoc_extensions = @config['pandoc']['extensions']
+    PandocRuby.new(content, *@pandoc_extensions).to_html5
+  end
+end
